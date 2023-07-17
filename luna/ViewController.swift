92cac86bd3ca701d804c753511f8aa5aeaff4e53
apfdamascena <apfd@cin.ufpe.br>
2023-07-17 19:54:51 -0300

    feature(base-project): setup base folder

---
+++ luna/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  luna
+//
+//  Created by alexdamascena on 17/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
