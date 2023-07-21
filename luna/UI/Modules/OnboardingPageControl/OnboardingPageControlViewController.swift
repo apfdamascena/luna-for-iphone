813ab0a53b4f3d7cb1c032772afd349a9d7b6b3f
apfdamascena <apfd@cin.ufpe.br>
2023-07-21 20:21:48 -0300

    feature(page-control): page control working  with obsevers

--- luna/UI/Modules/OnboardingPageControl/OnboardingPageControlViewController.swift
+++ luna/UI/Modules/OnboardingPageControl/OnboardingPageControlViewController.swift
@@ -24,20 +24,28 @@ class OnboardingPageControlDataSourceImpl: OnboardingPageControlDataSource {
     ]
 }
 
-class OnboardingPageControlViewController: UIViewController,
-                                           AnyView,
-                                           TouchableUserEvent {
+protocol DataSourceEventObservable {
     
+    func addDataSourceEventObservable()
+}
+
+class OnboardingPageControlViewController: UIPageViewController,
+                                           AnyView,
+                                           TouchableUserEvent,
+                                           DataSourceEventObservable {
+
     var presenter: ViewToPresenterOnboardingPageControlProtocol?
-    private var disposeBag = DisposeBag()
-    private let onboardingButtons = OnboardingButtonView()
     private var datasource: OnboardingPageControlDataSource?
     
+    private let onboardingButtons = OnboardingButtonView()
+    private var disposeBag = DisposeBag()
+    
     init(datasource: OnboardingPageControlDataSource){
-        super.init(nibName: nil, bundle: nil)
+        super.init(transitionStyle: .scroll, navigationOrientation: .horizontal)
         self.datasource = datasource
-        
+
         addUserTouchTrigger()
+        addDataSourceEventObservable()
     }
     
     required init?(coder: NSCoder) {
@@ -51,26 +59,39 @@ class OnboardingPageControlViewController: UIViewController,
         view.backgroundColor = .white
     }
     
-    func addUserTouchTrigger() {
-        
-        onboardingButtons.nextButton.rx.tap.bind {
-            guard let currentPage = try? self.datasource?.pageIndex.value() else { return }
-            self.datasource?.pageIndex.onNext(currentPage + 1)
-        }.disposed(by: disposeBag)
-    }
-    
     func addSubviews() {
         view.addSubview(onboardingButtons)
     }
     
     func addConstraints() {
         onboardingButtons.backgroundColor = .yellow
-        
+    
         onboardingButtons.snp.makeConstraints{
             $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-2.su)
             $0.leading.trailing.equalToSuperview()
             $0.height.equalTo(7.su)
         }
+    }
+    
+    func addUserTouchTrigger() {
+        
+        onboardingButtons.nextButton.rx.tap.bind {
+            guard let currentPage = try? self.datasource?.pageIndex.value() else { return }
+            self.datasource?.pageIndex.onNext(currentPage + 1)
+        }.disposed(by: disposeBag)
+        
+        onboardingButtons.backButton.rx.tap.bind {
+            guard let currentPage = try? self.datasource?.pageIndex.value() else { return }
+            self.datasource?.pageIndex.onNext(currentPage - 1)
+        }.disposed(by: disposeBag)
+        
+    }
+    
+    func addDataSourceEventObservable() {
+        datasource?.pageIndex.subscribe(onNext: { pageIndex in
+            guard let controller = self.datasource?.pages[pageIndex] else { return }
+            self.setViewControllers([controller], direction: .forward, animated: true)
+        }).disposed(by: disposeBag)
     }
 
 }
