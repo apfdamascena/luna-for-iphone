f8c5584da0143067684e2d86996e9d7681ff2ccf
Sofia Melo <sofianovica@hotmail.com>
2023-07-21 20:05:57 -0300

    feature(menstruation-duration): add picker-view

--- luna/UI/Modules/MenstruationDurantion/MenstruationDurationViewController.swift
+++ luna/UI/Modules/MenstruationDurantion/MenstruationDurationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RxSwift
 
 class MenstruationDurationViewController: UIViewController {
     
@@ -14,10 +15,31 @@ class MenstruationDurationViewController: UIViewController {
     
     let menstruationDurationView = MenstruationDurationView()
 
+    let disposeBag = DisposeBag()
+
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         view = menstruationDurationView
+        setupRx()
+    }
+    
+    func setupRx() {
+            Observable.just(["1","2", "3","4","5", "6","7", "8","9","10"])
+            .bind(to: menstruationDurationView.picker.rx.itemTitles) { _, item in
+                    return "\(item)"
+             }.disposed(by: disposeBag)
+
+        menstruationDurationView.picker.rx.itemSelected.asObservable().subscribe(onNext: {item in
+
+                //item.row gives you the index of the selected item, so do what you need with                  it
+
+                //also here you can call .resignFirstResponder() on whatever element brought up the pickerview (e.g. a button) in order to close the pickerview
+
+            }).disposed(by: disposeBag)
+
+        menstruationDurationView.picker.selectRow(4, inComponent: 0, animated: true)
+
     }
 
 }
@@ -29,6 +51,7 @@ extension MenstruationDurationViewController: PresenterToViewMenstruationDuratio
 
 #if DEBUG
 import SwiftUI
+import RxSwift
 
 @available(iOS 13, *)
 struct MenstruationDurationViewController_Preview: PreviewProvider {
