b4f5840c136189e4bcc72cd4080e8ecd162b31e2
apfdamascena <apfd@cin.ufpe.br>
2023-07-26 19:35:14 -0300

    feature(onboarding-manager): connect onboarding manager to flow

--- luna/SceneDelegate.swift
+++ luna/SceneDelegate.swift
@@ -20,7 +20,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let root = RootScreenViewFactory.createModule()
         window.rootViewController = root
         window.makeKeyAndVisible()
-        
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
