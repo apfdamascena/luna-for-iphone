e8128001254616d1766183e53027818686ba0af6
apfdamascena <apfd@cin.ufpe.br>
2023-08-03 20:50:33 -0300

    feature(home-scroll): add segmented line

--- luna/UI/Modules/Home/Views/CardHomeInformation.swift
+++ luna/UI/Modules/Home/Views/CardHomeInformation.swift
@@ -8,52 +8,6 @@
 import UIKit
 import SnapKit
 
-class SegmentedLine: UIView, AnyView {
-    
-    private let ball: UIImageView = {
-        let view = UIImageView(image: Asset.ball.image)
-        return view
-    }()
-    
-    private let line: UIView = {
-        let view = UIView()
-        view.backgroundColor = Asset.gray200.color
-        return view
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupView()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    func addSubviews() {
-        
-        addSubview(ball)
-        addSubview(line)
-    }
-    
-    func addConstraints() {
-        
-        ball.snp.makeConstraints{
-            $0.centerX.top.equalToSuperview()
-        }
-        
-        line.snp.makeConstraints{
-            $0.top.centerX.equalTo(ball)
-            $0.bottom.equalToSuperview()
-            $0.width.equalTo(1)
-        }
-    }
-    
-    func addAdditionalConfiguration() {
-        ball.layer.zPosition = 13
-    }
-}
-
 class CardHomeInformation: UIView, AnyView {
     
     private let subtitleContainer = UIView()
