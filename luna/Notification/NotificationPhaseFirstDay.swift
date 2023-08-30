720775e4443048c23e1ab2ede088e1bf16321c17
apfdamascena <apfd@cin.ufpe.br>
2023-08-30 20:13:02 -0300

    feature(notification): create factory to create notification body

--- luna/Notification/NotificationPhaseFirstDay.swift
+++ luna/Notification/NotificationPhaseFirstDay.swift
@@ -8,30 +8,6 @@
 import Foundation
 import UserNotifications
 
-//NotificationBody
-class NotificationFactoryMessage {
-    
-    static func create(for phase: CyclePhase) -> NotificationBody {
-        switch phase {
-        case .fertile:
-            return NotificationBody(title: "Cheguei, mores  💅" ,
-                                    body: "Fase ovulatória à vista! A boa é evitar estresses hoje, hein? Saiba mais ")
-        case .folicular:
-            return NotificationBody(title: "Chegou a famosa ⚡️", body: "Ela mesmo, a Fase Folicular! Energia pode estar em alta hoje. Veja como aproveitar!")
-            
-        case .luteal:
-            return NotificationBody(title: "Piscou e eu cheguei",
-                                    body: "Fase Lútea on! Hormônios em alta. Saiba o que pode acontecer nessa fase 👉")
-            
-        case .luteal:
-            return NotificationBody(title: "Sem gracinha hoje ", body: "Sintomas da TPM podem aparecer nos próximos dias. Saiba mais sobre essa fase 👉")
-            
-        default:
-            return NotificationBody(title: "", body: "")
-        }
-    }
-}
-
 
 class NotificationPhaseFirstDay: NotificationFilterCommand {
     
