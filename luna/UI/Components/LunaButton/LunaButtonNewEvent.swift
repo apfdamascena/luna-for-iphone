0eacfba716611f6fb24102a083d7564a67f99b4a
CynaraCosta <cvocac@cin.ufpe.br>
2023-09-11 15:20:05 -0300

    feature(new-task-button): added the newTaskButton

--- luna/UI/Components/LunaButton/LunaButtonNewEvent.swift
+++ luna/UI/Components/LunaButton/LunaButtonNewEvent.swift
@@ -29,7 +29,7 @@ class LunaButtonNewEvent: DrawableButton {
         
         configuration.image = Asset.plusIcon.image
         configuration.imagePadding = 1.su
-//        configuration.contentInsets = NSDirectionalEdgeInsets(top: 1.su, leading: (1.2).su, bottom: 1.su, trailing: (11).su)
+        configuration.contentInsets = NSDirectionalEdgeInsets(top: 1.su, leading: 0.su, bottom: 1.su, trailing: 0.su)
 
         
         configuration.background.backgroundColor = .clear
