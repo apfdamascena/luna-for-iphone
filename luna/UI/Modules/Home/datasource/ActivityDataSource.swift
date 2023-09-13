d2af7e3f9d588635de982ccbedecb4d0a6cc720f
apfdamascena <apfd@cin.ufpe.br>
2023-09-13 16:52:32 -0300

    feature(activities-table): create datasource protocol

---
+++ luna/UI/Modules/Home/datasource/ActivityDataSource.swift
@@ -0,0 +1,14 @@
+//
+//  ActivityDatasource.swift
+//  luna
+//
+//  Created by alexdamascena on 13/09/23.
+//
+
+import RxSwift
+
+protocol ActivityDataSource {
+    
+    var activities: BehaviorSubject<[String]> { get }
+
+}
