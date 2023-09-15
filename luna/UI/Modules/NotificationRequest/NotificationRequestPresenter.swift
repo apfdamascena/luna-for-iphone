77e3969a41f102c29ee10f9b1a075f0419e30ecc
apfdamascena <apfd@cin.ufpe.br>
2023-09-15 15:27:34 -0300

    feature(onboarding-notification): create files

---
+++ luna/UI/Modules/NotificationRequest/NotificationRequestPresenter.swift
@@ -0,0 +1,15 @@
+//
+//  NotificationRequestPresenter.swift
+//  luna
+//
+//  Created by alexdamascena on 15/09/23.
+//  
+//
+
+import Foundation
+
+class NotificationRequestPresenter: ViewToPresenterNotificationRequestProtocol {
+
+    var view: PresenterToViewNotificationRequestProtocol?
+    var router: PresenterToRouterNotificationRequestProtocol?
+}
---
+++ luna/UI/Modules/NotificationRequest/NotificationRequestViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationRequestViewController.swift
+//  luna
+//
+//  Created by alexdamascena on 15/09/23.
+//  
+//
+
+import UIKit
+
+class NotificationRequestViewController: UIViewController {
+    
+    var presenter: ViewToPresenterNotificationRequestProtocol?
+    
+    // MARK: - Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extension NotificationRequestViewController: PresenterToViewNotificationRequestProtocol{
+    // TODO: Implement View Output Methods
+}
+
+
+#if DEBUG
+import SwiftUI
+
+@available(iOS 13, *)
+struct NotificationRequestViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        NotificationRequestViewController().showPreview()
+    }
+}
+
+#endif
+
+
+
